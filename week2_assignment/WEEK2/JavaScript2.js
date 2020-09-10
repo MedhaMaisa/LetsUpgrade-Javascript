96aaf21e3f043e05d9f79466be8d99d7c789ad27
MedhaMaisa <52379661+MedhaMaisa@users.noreply.github.com>
2020-09-10 11:54:36 +0530

    Add files via upload

---
+++ week2_assignment/WEEK2/JavaScript2.js
@@ -0,0 +1,5 @@
+﻿function copy() {
+    var n1 = document.getElementById('t1');
+    var n2 = document.getElementById('t2');
+    n2.value = n1.value;
+}
---
+++ week2_assignment/WEEK2/JavaScript3.js
@@ -0,0 +1,31 @@
+﻿let people = [
+    { name: "M", age: 31, country: "India", Hobbies: "KABADDI" }, { name: "c", age: 41, country: "FRANCE", Hobbies: "SKATING" },
+    { name: "B", age: 35, country: "US", Hobbies: "CHESS" }, { name: "D", age: 29, country: "India", Hobbies: "HOCKEY" },
+    { name: "A", age: 20, country: "NEW ZEALAND", Hobbies: "cricket" }
+]
+document.write("OBJECTS IN THE ARRAY");
+document.write("<br />");
+people.forEach(function (a) {
+        document.write(JSON.stringify(a));
+})
+document.write("<br />");
+
+document.write("<br />");
+document.write("OBJECTS WITH AGE LESS THAN 30");
+document.write("<br />");
+people.forEach(function(a)
+{
+    if (a.hasOwnProperty("age") && a["age"] < 30) {
+        document.write(JSON.stringify(a));
+    }
+})
+document.write("<br />");
+
+document.write("<br />");
+document.write("OBJECTS WITH COUNTRY INDIA\n");
+document.write("<br />");
+people.forEach(function (a) {
+    if (a.hasOwnProperty("country") && a["country"] == "India") {
+        document.write(JSON.stringify(a));
+    }
+})
